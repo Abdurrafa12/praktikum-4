ca80fa572d8842e92a36d1745d124cfdfde1f013
Aabdurrafa <146910730+Abdurrafa12@users.noreply.github.com>
2024-05-20 09:15:30 +0700

    first commit

---
+++ admin/daftar.php
@@ -0,0 +1,15 @@
+<?php
+require_once './top.php';
+require_once './menu.php';
+?>
+
+<div class="container-fluid px-4">
+                        <h1 class="mt-4">Silakan Isi Form Pendaftaran</h1>
+
+
+                     //form daftar//
+                    </div>
+
+<?php
+require_once './bottom.php';
+?>
